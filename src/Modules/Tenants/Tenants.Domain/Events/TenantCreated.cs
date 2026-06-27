fa37a21045e0090273e3b3b131f66b7c2de072e2
Faizan Ahmed Khan R K <faizanahmedkhan005@gmail.com>
2026-06-27 11:33:24 -0400

    Created TenantCreated Event

---
+++ src/Modules/Tenants/Tenants.Domain/Events/TenantCreated.cs
@@ -0,0 +1,12 @@
+﻿using BuildingBlocks.Domain;
+
+namespace Tenants.Domain.Events;
+
+public sealed record TenantCreated : IDomainEvent
+{
+    public required TenantId TenantId { get; init; }
+    public required string Name { get; init; }
+    public required string Slug { get; init; }
+    public required Plan Plan { get; init; }
+    public required DateTimeOffset OccurredAt { get; init; }
+}
