ac43b219b564d4daa54578821cc8735499c83860
Faizan Ahmed Khan R K <faizanahmedkhan005@gmail.com>
2026-06-27 11:15:13 -0400

    Created ICommandQuery and I Command Query Handlers

---
+++ src/BuildingBlocks/BuildingBlocks.Application/ICommandQueryHandlers.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.Application;
+
+public interface ICommandHandler<in TCommand, TResponse>
+    : IRequestHandler<TCommand, TResponse>
+    where TCommand : ICommand<TResponse>;
+
+public interface ICommandHandler<in TCommand>
+    : IRequestHandler<TCommand, Unit>
+    where TCommand : ICommand;
+
+public interface IQueryHandler<in TQuery, TResponse>
+    : IRequestHandler<TQuery, TResponse>
+    where TQuery : IQuery<TResponse>;
