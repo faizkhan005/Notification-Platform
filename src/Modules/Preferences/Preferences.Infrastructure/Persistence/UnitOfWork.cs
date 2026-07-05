ab57c33e0861c32728571fe88cd1c48eba007b9c
Faizan Ahmed Khan R K <faizanahmedkhan005@gmail.com>
2026-07-05 16:32:24 -0400

    Completed Preferences.Infrastructure

---
+++ src/Modules/Preferences/Preferences.Infrastructure/Persistence/UnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using Preferences.Application;
+
+namespace Preferences.Infrastructure.Persistence;
+
+public sealed class UnitOfWork : IPreferencesUnitOfWork
+{
+    private readonly PreferencesDbContext _context;
+
+    public UnitOfWork(PreferencesDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        => await _context.SaveChangesAsync(cancellationToken);
+}
